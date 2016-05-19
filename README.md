f75ec417d2ff9bfdce8df511bf3078a01333ce39
Michael Gulev <greenday2.gd@gmail.com>
2016-05-19 15:23:44 +0700

    Fix errors.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# wt-tests
+wt-tests
+gunicorn myproject.wsgi
+This will start one process running one thread listening on 127.0.0.1:8000.
+It requires that your project be on the Python path; the simplest
+way to ensure that is to run this command from the same directory as your
+manage.py file.
+
+
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,12 +15,12 @@ urlpatterns = patterns(
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', p404, name='root'),
+    url(r'^$', test, name='root'),
     url(r'^login/', p404, name='login'),
     url(r'^signup/', p404, name='signup'),
     url(r'^ask/', p404, name='ask'),
     url(r'^popular/', p404, name='popular'),
     url(r'^new/', p404, name='new'),
-    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
+    url(r'^question/(?P<id>.*)/$', test, name='question'),
     url(r'^admin/', include(admin.site.urls)),
 )
