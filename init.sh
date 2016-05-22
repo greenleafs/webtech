241f2eeadf2ff6efe27177be066a14b7306e1f57
Michael Gulev <greenday2.gd@gmail.com>
2016-05-22 22:56:22 +0700

    Improve init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+git clone https://github.com/greenleafs/webtech.git web
+cd web
+cp ask/ask/default_settings.py ask/ask/settings.py
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/test-nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo service mysql start
+mysql -uroot -e "create database wtech"
+cd ask
+./manage.py syncdb --noinput
+gunicorn -b 0.0.0.0:8000 ask.wsgi
